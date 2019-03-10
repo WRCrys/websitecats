dfe52616eb84bbd49130ffd63d5afe96fcd4a71a
Crystyano Almeida <winnerbyjesus@gmail.com>
2019-03-10 16:42:07 -0300

    frist commit

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Website Cats</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/stylesheets.css">
+    <!--jQuery-->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>    
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">The Best <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">All You Need</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Energy Saving</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div id="home">
+        <div class="landing-text">
+            <h1>bootstrap</h1>
+        </div>
+    </div>
+    <div class="padding">
+        <div class="container">`
+            <div class="row">
+                <div class="col-sm-6">
+                    <img class="img-thumbnail" alt="" src="assets/images/adoravel-animal-animal-de-estimacao-1170986.jpg">
+                </div>
+                <div class="col-sm-6 text-center">
+                    <p class="lead">Loren ipsum dolor sit amet, consectetur adipiscing elit. Integer Nec odio. Praesent cursus
+                         ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis 
+                         ipsum. Preasent mauris. Fusce nec tellus sed augue semper porta. Marius massa. Vestibulum 
+                         inia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, 
+                         per inceptos himenaeos.</p>
+                         <!---->
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+</body>
+</html>
