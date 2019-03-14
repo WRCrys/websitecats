4d69f2fe76c1be08b22cb2181e417357286413a5
Crystyano <winnerbyjesus@gmail.com>
2019-03-13 23:41:32 -0300

    version 2.0

--- index.php
+++ index.php
@@ -7,15 +7,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="css/stylesheets.css">
     <!--jQuery-->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>    
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>   
+<!--    Meu CSS-->
+    <link href="css/stylesheets.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">Navbar</a>
+    <nav class="navbar navbar-expand-lg navbar-dark">
+        <a class="navbar-brand" href="#"></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -37,30 +38,162 @@
         </div>
     </nav>
 
-    <div id="home">
+<!--    <header class="bg-image">-->
+    <div class="" id="home">
         <div class="landing-text">
-            <h1>bootstrap</h1>
+            <h1 class="title">In ancient times cats were</h1>
+            <h1 class="title">worshipped as gods; they</h1>
+            <h1 class="title">have not forgotten this.</h1>
+        </div>
+    </div>
+<!--    </header>-->
+    <div class="container">
+        <div class="row">
+            <div id="allyouneed" class="container col-12 text-center">
+                <br>
+                <h2>CATS ARE THE BEST</h2>
+                <br>
+            </div>
+            
         </div>
     </div>
     <div class="padding">
-        <div class="container">`
+        <div class="container">
             <div class="row">
-                <div class="col-sm-6">
-                    <img class="img-thumbnail" alt="" src="assets/images/adoravel-animal-animal-de-estimacao-1170986.jpg">
+                <div class="col-sm-4">
+                    <img class="img-thumbnail rounded float-left" alt="100" src="assets/images/cat-7.jpg">
                 </div>
-                <div class="col-sm-6 text-center">
+                <div class="col-sm-4">
+                    <img class="img-thumbnail rounded float-left" alt="100" src="assets/images/cat-5.jpg">
+                </div>
+                <div class="col-sm-4">
+                    <img class="img-thumbnail rounded float-left" alt="100" src="assets/images/cat-6.jpg">
+                </div>
+            </div>
+        </div>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-4">
+				Loren ipsum dolor sit amet, consectetur adipiscing elit. Integer Nec odio. Praesent cursus
+                         ante dapibus diam.
+			</div>
+			<div class="col-sm-4">
+				Loren ipsum dolor sit amet, consectetur adipiscing elit. Integer Nec odio. Praesent cursus
+                         ante dapibus diam.
+			</div>
+			<div class="col-sm-4">
+				Loren ipsum dolor sit amet, consectetur adipiscing elit. Integer Nec odio. Praesent cursus
+                         ante dapibus diam.
+			</div>
+		</div>
+	</div>
+    </div>
+    <div class="padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 text-center">
                     <p class="lead">Loren ipsum dolor sit amet, consectetur adipiscing elit. Integer Nec odio. Praesent cursus
                          ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis 
                          ipsum. Preasent mauris. Fusce nec tellus sed augue semper porta. Marius massa. Vestibulum 
                          inia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, 
                          per inceptos himenaeos.</p>
-                         <!---->
+                </div>
+            </div>
+        </div>
+    </div>
+   <div class="padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8 text-center">
+                    <br>
+                    <p class="">Loren ipsum dolor sit amet, consectetur adipiscing elit. Integer Nec odio. Praesent cursus
+                         ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis 
+                         ipsum. Preasent mauris. Fusce nec tellus sed augue semper porta. Marius massa. Vestibulum 
+                         inia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, 
+                         per inceptos himenaeos.</p>
+                </div>
+                <div class="col-sm-4">
+                    <img class="img-thumbnail rounded float-left" alt="100" src="assets/images/cat-1.jpg">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8 text-center">
+                    <br>
+                    <p class="">Loren ipsum dolor sit amet, consectetur adipiscing elit. Integer Nec odio. Praesent cursus
+                         ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis 
+                         ipsum. Preasent mauris. Fusce nec tellus sed augue semper porta. Marius massa. Vestibulum 
+                         inia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, 
+                         per inceptos himenaeos.</p>
+                </div>
+                <div class="col-sm-4">
+                    <img class="img-thumbnail rounded float-left" alt="100" src="assets/images/cat-2.jpg">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8 text-center">
+                    <br>
+                    <p class="">Loren ipsum dolor sit amet, consectetur adipiscing elit. Integer Nec odio. Praesent cursus
+                         ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis 
+                         ipsum. Preasent mauris. Fusce nec tellus sed augue semper porta. Marius massa. Vestibulum 
+                         inia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, 
+                         per inceptos himenaeos.</p>
+                </div>
+                <div class="col-sm-4">
+                    <img class="img-thumbnail rounded float-left" alt="100" src="assets/images/cat-3.jpg">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8 text-center">
+                    <br>
+                    <p class="">Loren ipsum dolor sit amet, consectetur adipiscing elit. Integer Nec odio. Praesent cursus
+                         ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis 
+                         ipsum. Preasent mauris. Fusce nec tellus sed augue semper porta. Marius massa. Vestibulum 
+                         inia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, 
+                         per inceptos himenaeos.</p>
+                </div>
+                <div class="col-sm-4">
+                    <img class="img-thumbnail rounded float-left" alt="100" src="assets/images/cat-4.jpg">
                 </div>
             </div>
         </div>
     </div>
 
-
-
+<!--Div do gato branco-->
+    <div id="white-cat">
+        <div class="container landing-text text-left">
+            <h1>You can teach a cat to do anything...</h1>
+		<h1>that it wants to do.</h1>
+		<input type="submit" class="btncontact" value="Contact Us"/>
+        </div>
+    </div>
+<footer class="container-fluid text-center">
+    <div class="row">
+        <div class="col-sm-4">
+            <h4>The Best</h4>
+            <h4>All You Need</h4>
+            <h4>Energy Saving</h4>
+        </div>
+        <div class="col-sm-4">
+            <h5>Meow Avenue, 1565</h5>
+            <h5>+55 85 0000-0000</h5>
+            <h5>meow@gmail.com</h5>
+            <a class="fa fa-facebook" href="#"></a>
+        </div>
+        <div class="col-sm-4">
+        </div>
+    </div>
+</footer>
 </body>
 </html>
